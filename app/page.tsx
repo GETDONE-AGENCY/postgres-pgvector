6fd3822b1944035003a3e73f73de10e3b0e8cdb2
web3some@gmail.com <web3some@gmail.com>
2024-04-09 16:49:22 +0200

    INE

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,11 @@ export default function Home() {
         href="https://vercel.com/templates/next.js/postgres-pgvector"
         className="group rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
       >
-        <p>Deploy your own to Vercel</p>
+        <p>Podcastbible</p>
         <ExpandingArrow />
       </Link>
       <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
-        Postgres on Vercel
+        Podcastbible
       </h1>
       <div className="bg-white/30 p-6 lg:p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
         <div className="flex justify-between items-center mb-4">
